8bc0816e6ebe5c0302467b4e75b5d5cf3434484b
Asmaa <Asmaa>
2017-06-02 16:01:38 +0100

    Bug header fixed

--- app.js
+++ app.js
@@ -24,6 +24,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+app.set('templates', path.join(__dirname, 'templates'));
+
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
